2bdc779a124f4268b471f387601cbc2080dab4e8
Anuja Kumari <166990955+k-anuja06@users.noreply.github.com>
2025-04-23 11:30:16 +0000

    service-worker

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-      navigator.serviceWorker
-        .register('service-worker.js') // Path relative to HTML file
-        .then(function (registration) {
-          console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(function (error) {
-          console.log('Service Worker registration failed:', error);
-        });
-    });
-  }
-  
+  window.addEventListener('load', function () {
+    navigator.serviceWorker
+      .register('service-worker.js')  // Correct path now since it's in root
+      .then(function (registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(function (error) {
+        console.log('Service Worker registration failed:', error);
+      });
+  });
+}
